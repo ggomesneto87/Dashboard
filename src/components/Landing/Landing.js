12c15bdbf8f0fdf077a633fcb9b80cba0bd9e6e4
root <ggomesneto@hotmail.com>
2023-01-18 14:10:46 -0800

    first commit

---
+++ src/components/Landing/Landing.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "./css/Landing.css";
+
+import Menu from "../Menu/Menu";
+
+const Landing = () => {
+  return (
+    <div className="landing-wrapper">
+      <Menu />
+    </div>
+  );
+};
+
+export default Landing;
---
+++ src/components/Menu/Menu.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import "./css/Menu.css";
+
+const Menu = () => {
+  const items = ["Home", "About Us", "Login"];
+  let links = [];
+
+  for (let i = 0; i < items.length; i++) {
+    links.push(<div className="menu-link">{items[i]}</div>);
+  }
+
+  return <div className="menu-wrapper">{links}</div>;
+};
+
+export default Menu;
