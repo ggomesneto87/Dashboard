7331db8d071b252bcaea6953f58589fc5dce7f4c
ogeidazuos <114941377+ogeidazuos@users.noreply.github.com>
2023-01-19 17:20:48 -0300

    LandLeft

---
+++ src/components/Landing/LandLeft/LandLeft.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+import "./css/LandLeft.css";
+import leftImg from "../../Images/land-left-img.png";
+
+const LandLeft = () => {
+  return (
+    <div className="land-left-wrapper">
+             
+        <div className="land-left-text">
+          <div className="img-left-wrapper">
+            <img src={leftImg} className="land-left-img" alt="land left"></img>
+            </div>
+              <div className="left-text"> Customize your Dasboard, gather real-time data and closely monitor the information that best serves your e-commerce!
+              </div>
+            </div>
+          </div>
+  );
+};
+
+export default LandLeft;
--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -4,12 +4,16 @@ import "./css/Landing.css";
 
 import Menu from "../Menu/Menu";
 import LandRight from "./LandRight/LandRight";
+import LandLeft from "./LandLeft/LandLeft";
 
 const Landing = () => {
   return (
     <div className="landing-wrapper">
       <Menu />
-      <LandRight />
+      <div className="text-wrapper">
+        <LandRight />
+        <LandLeft />
+        </div>      
     </div>
   );
 };
