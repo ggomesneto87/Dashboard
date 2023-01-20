ed091c6677308587af34b0f79a713a2d44a037fa
root <ggomesneto@hotmail.com>
2023-01-20 11:11:27 -0800

    removing link css

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -15,7 +15,10 @@ const Menu = () => {
 
   for (let i = 0; i < items.length; i++) {
     links.push(
-      <NavLink to={items[i][1]}>
+      <NavLink
+        to={items[i][1]}
+        style={{ textDecoration: "none", color: "black" }}
+      >
         {" "}
         <div className="menu-link">{items[i][0]}</div>
       </NavLink>
@@ -26,7 +29,9 @@ const Menu = () => {
     <div className="menu-wrapper">
       <div className="logo-wrapper">
         <img src={logo} className="logo-img"></img>
-        <span className="logo-name">Dashboard</span>
+        <NavLink to="/" style={{ textDecoration: "none", color: "black" }}>
+          <span className="logo-name">Dashboard</span>
+        </NavLink>
       </div>
       <div className="sign-in">Sign Up</div>
       <div className="link-wrapper">{links}</div>
