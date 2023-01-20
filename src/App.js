1e08dff4fb604d4fed6e5ac59e3938e2eb6a9a2f
ogeidazuos <114941377+ogeidazuos@users.noreply.github.com>
2023-01-19 21:24:16 -0300

    pricing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import "./App.css";
 
 import Landing from "./components/Landing/Landing";
+import Pricing from "./components/Pricing/Pricing";
 
 function App() {
   return (
     <div className="App">
-      <Landing />
+      <Pricing />
     </div>
   );
 }
---
+++ src/components/Pricing/Pricing.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./css/Pricing.css";
+
+import Menu from "../Menu/Menu"
+
+const Pricing = () => {
+  return (
+    <Menu />
+  )
+};
+
+export default Pricing;
